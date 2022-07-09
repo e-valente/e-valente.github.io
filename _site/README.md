fab5d1afa722f77667c3d7d1d28e32ec58f89e6d
Emanuel Valente <emanuel.valente@ifood.com.br>
2022-07-09 15:31:03 -0300

    update content

--- _site/README.md
+++
@@ -1,2 +0,0 @@
-# animal-blog
-Animal's Blog repository
--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "hello world!",
-        "excerpt":"Hi folks, here I’ll try to write what I’m doing during my spare time …  ","categories": ["testing"],
-        "tags": [],
-        "url": "/testing/2021/06/12/hello-world.html",
-        "teaser": null
-      }]
